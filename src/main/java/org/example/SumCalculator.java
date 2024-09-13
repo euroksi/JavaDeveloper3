45b3e028e93ff98b351a4dfb03cc1725ef85d547
euroksi <153302244+euroksi@users.noreply.github.com>
2024-09-13 10:41:26 +0200

    javadeveloper3

---
+++ src/main/java/org/example/SumCalculator.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class SumCalculator {
+    public int sum(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("n must be greater than 0");
+        }
+        return n * (n + 1) / 2;
+    }
+}
